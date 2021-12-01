1c58b4b90142eee9f5cc78a2ed7469465e3d71c8
Zect <57027411+sas08@users.noreply.github.com>
2021-12-01 14:34:55 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -1,21 +1,39 @@
-from discord.ext import commands
+import discord
 from os import getenv
 import traceback
 
-bot = commands.Bot(command_prefix='/')
+class MyClient(discord.Client):
+  async def on_ready(self):
+    self.user_list = {}
+    print(f'Logged on as {self.user}!')
+    print("------")
 
+  async def on_message(self, message):
+    if message.author.id == self.user.id:
+        return
 
-@bot.event
-async def on_command_error(ctx, error):
-    orig_error = getattr(error, "original", error)
-    error_msg = ''.join(traceback.TracebackException.from_exception(orig_error).format())
-    await ctx.send(error_msg)
+    guildId = str(message.guild.id)
 
+    if guildId not in self.user_list:
+      self.user_list[guildId] = []
 
-@bot.command()
-async def ping(ctx):
-    await ctx.send('pong')
+    if message.content.startswith('n.join'):
+      self.user_list[guildId].append(message.author.id)
 
+    if message.content.startswith('n.leave'):
+      self.user_list[guildId].remove(message.author.id)
+
+    msg = f'Message from **{message.author}** in {message.channel.name}:\n{message.content}'
+
+    for id in self.user_list[guildId]:
+      mem = message.guild.get_member(id)
+      if not mem:
+        print(f'user {id} was not found')
+        continue
+      await mem.send(msg)
+
+intents = discord.Intents.all()
+client = MyClient(intents=intents)
 
 token = getenv('DISCORD_BOT_TOKEN')
-bot.run(token)
+client.run(token)
